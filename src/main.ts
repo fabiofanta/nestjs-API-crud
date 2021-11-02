c2a9ce4f0c132cf1a80fd705059efc3f48fc1bad
fabiofanta <f.fantacuzzi@gmail.com>
2021-11-02 16:48:59 +0100

    swagger UI OpenAPI added

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory} from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { TokenBlacklistService } from './token-blacklist/token-blacklist.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 
 
@@ -9,6 +10,13 @@ async function bootstrap() {
   TokenBlacklistService.initBlackList();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Easypol API')
+    .setDescription('Easypol API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
